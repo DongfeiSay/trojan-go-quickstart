b8a35b33a8a3e2c29c754909de888a99c5e37628
DongfeiSay <49421020+DongfeiSay@users.noreply.github.com>
2020-06-14 17:53:03 +0800

    Pre operation verification

--- trojan-go-maintenance.sh
+++ trojan-go-maintenance.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if [[ $(id -u) != 0 ]]; then
+    echo "请以超级用户身份运行此脚本。"
+    exit 1
+fi
+
+if [[ $(uname -m 2> /dev/null) != x86_64 ]]; then
+    echo "请在x86_64机器上运行此脚本。"
+    exit 1
+fi
+
 _INSTALL(){
 	if [ -f /etc/centos-release ]; then
 		yum install -y wget curl zip
@@ -14,18 +24,16 @@ _INSTALL(){
 	systemctl daemon-reload
 	systemctl restart trojan-go
 	systemctl enable trojan-go
-	echo "安装完成"
+	echo "安装完成！"
 }
-
 
 _UPDATE(){
 	systemctl stop trojan-go
 	rm -rf /usr/bin/trojan-go/*
 	wget -N --no-check-certificate https://github.com/p4gefau1t/trojan-go/releases/download/$(curl -fsSL https://api.github.com/repos/p4gefau1t/trojan-go/releases | grep '"tag_name":' | head -n 1 | sed -E 's/.*"([^"]+)".*/\1/')/trojan-go-linux-amd64.zip && unzip -d /usr/bin/trojan-go/ ./trojan-go-linux-amd64.zip && chmod +x /usr/bin/trojan-go/trojan-go && rm -rf ./trojan-go-linux-amd64.zip
 	systemctl restart trojan-go
-	echo "升级完成"
+	echo "升级完成！"
 }
-
 
 _UNINSTALL(){
 	systemctl stop trojan-go
@@ -33,9 +41,8 @@ _UNINSTALL(){
 	rm -rf /usr/bin/trojan-go /etc/trojan-go
 	rm -rf /etc/systemd/system/trojan-go.service
 	systemctl daemon-reload
-	echo "卸载完成"
+	echo "卸载完成！"
 }
-
 
 echo "1.安装trojan-go"
 echo "2.升级trojan-go"
