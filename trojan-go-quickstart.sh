3ccb9a6466ad82901fa2e7fb3f974b33596bf88f
DongfeiSay <49421020+DongfeiSay@users.noreply.github.com>
2020-06-26 13:02:15 +0800

    Synchronize upstream installation path

--- trojan-go-quickstart.sh
+++ trojan-go-quickstart.sh
@@ -6,13 +6,12 @@ _INSTALL(){
 	else
 		apt install -y wget curl zip
 	fi
-	mkdir /etc/trojan-go
 	mkdir /usr/bin/trojan-go
-	wget -N --no-check-certificate https://github.com/p4gefau1t/trojan-go/releases/download/$(curl -fsSL https://api.github.com/repos/p4gefau1t/trojan-go/releases | grep '"tag_name":' | head -n 1 | sed -E 's/.*"([^"]+)".*/\1/')/trojan-go-linux-amd64.zip && unzip -d /usr/bin/trojan-go/ ./trojan-go-linux-amd64.zip && chmod +x /usr/bin/trojan-go/trojan-go && rm -rf ./trojan-go-linux-amd64.zip
-	cp /usr/bin/trojan-go/example/server.json /etc/trojan-go/config.json
-	cp /usr/bin/trojan-go/example/trojan-go.service /etc/systemd/system/trojan-go.service
+	mkdir /usr/lib/trojan-go
+	wget -N --no-check-certificate https://github.com/p4gefau1t/trojan-go/releases/download/$(curl -fsSL https://api.github.com/repos/p4gefau1t/trojan-go/releases | grep '"tag_name":' | head -n 1 | sed -E 's/.*"([^"]+)".*/\1/')/trojan-go-linux-amd64.zip && unzip -d /usr/lib/trojan-go/ ./trojan-go-linux-amd64.zip && mv /usr/lib/trojan-go/trojan-go /usr/bin/ && chmod +x /usr/bin/trojan-go && rm -rf ./trojan-go-linux-amd64.zip
+	cp /usr/lib/trojan-go/example/server.json /etc/trojan-go/config.json
+	cp /usr/lib/trojan-go/example/trojan-go.service /etc/systemd/system/trojan-go.service
 	systemctl daemon-reload
-	systemctl restart trojan-go
 	systemctl enable trojan-go
 	echo Done!
 }
