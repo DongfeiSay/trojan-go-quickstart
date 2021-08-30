0e7b7432d47021386680d0e4763cc427f48d43c7
DongfeiSay <49421020+DongfeiSay@users.noreply.github.com>
2021-08-30 21:27:31 +0800

    Update to the correct path

--- trojan-go-quickstart.sh
+++ trojan-go-quickstart.sh
@@ -7,10 +7,10 @@ _INSTALL(){
 		apt install -y wget curl zip
 	fi
 	mkdir /etc/trojan-go
-	mkdir /usr/lib/trojan-go
-	wget -N --no-check-certificate https://github.com/p4gefau1t/trojan-go/releases/download/$(curl -fsSL https://api.github.com/repos/p4gefau1t/trojan-go/releases | grep '"tag_name":' | head -n 1 | sed -E 's/.*"([^"]+)".*/\1/')/trojan-go-linux-amd64.zip && unzip -d /usr/lib/trojan-go/ ./trojan-go-linux-amd64.zip && mv /usr/lib/trojan-go/trojan-go /usr/bin/ && chmod +x /usr/bin/trojan-go && rm -rf ./trojan-go-linux-amd64.zip
-	cp /usr/lib/trojan-go/example/server.json /etc/trojan-go/config.json
-	cp /usr/lib/trojan-go/example/trojan-go.service /etc/systemd/system/trojan-go.service
+	mkdir /usr/share/trojan-go
+	wget -N --no-check-certificate https://github.com/p4gefau1t/trojan-go/releases/download/$(curl -fsSL https://api.github.com/repos/p4gefau1t/trojan-go/releases | grep '"tag_name":' | head -n 1 | sed -E 's/.*"([^"]+)".*/\1/')/trojan-go-linux-amd64.zip && unzip -d /usr/share/trojan-go/ ./trojan-go-linux-amd64.zip && mv /usr/share/trojan-go/trojan-go /usr/bin/ && chmod +x /usr/bin/trojan-go && rm -rf ./trojan-go-linux-amd64.zip
+	cp /usr/share/trojan-go/example/server.json /etc/trojan-go/config.json
+	cp /usr/share/trojan-go/example/trojan-go.service /etc/systemd/system/trojan-go.service
 	systemctl daemon-reload
 	systemctl enable trojan-go
 	echo Done!
